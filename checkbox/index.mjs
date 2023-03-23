5ea7fcfe9d19b1ddf9dcaa36b2dce55abe0993e9
Tierney Cyren <hello@bnb.im>
2023-03-23 22:57:56 +0900

    fix: run linter
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- checkbox/index.mjs
+++ checkbox/index.mjs
@@ -3,9 +3,7 @@
 import { collectBlocks, executeBlocks } from '@doctes/core'
 import { parseArgs } from 'node:util'
 import { readdirSync, statSync } from 'node:fs'
-import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
-
 
 // mostly inspired by this article: https://coderrocketfuel.com/article/recursively-list-all-the-files-in-a-directory-using-node-js
 async function getAllFiles (dirPath, arrayOfFiles) {
@@ -14,7 +12,7 @@ async function getAllFiles (dirPath, arrayOfFiles) {
   arrayOfFiles = arrayOfFiles || []
 
   files.forEach(function (file) {
-    if(statSync(`${dirPath}/${file}`).isDirectory()) {
+    if (statSync(`${dirPath}/${file}`).isDirectory()) {
       arrayOfFiles = getAllFiles(`${dirPath}/${file}`, arrayOfFiles)
     } else {
       arrayOfFiles.push(fileURLToPath(new URL(`${dirPath}/${file}`, import.meta.url)))
@@ -29,39 +27,39 @@ const options = {
     type: 'boolean'
   },
   dir: {
-    type: 'string',
+    type: 'string'
   },
   file: {
-    type: 'string',
+    type: 'string'
   },
   silent: {
     type: 'boolean'
   }
 }
 
-if(!process.argv.includes('--dir') && !process.argv.includes('--file')) {
+if (!process.argv.includes('--dir') && !process.argv.includes('--file')) {
   process.argv.push('--dir=./')
 }
 
-const { values, positionals } = parseArgs({ options })
+const { values } = parseArgs({ options })
 
 async function execute () {
-  if(values.file && values.dir) {
+  if (values.file && values.dir) {
     console.log('Please choose either a file or a directory, not both.')
     process.exitCode(1)
   }
 
-  if(values.file && !values.dir) {
+  if (values.file && !values.dir) {
     const blocks = await collectBlocks(values.file)
     const executedBlocks = await executeBlocks(blocks)
     logCheck(executedBlocks, values.file, values)
   }
 
-  if(values.dir && !values.file) {
+  if (values.dir && !values.file) {
     const normalizedDir = fileURLToPath(new URL(values.dir, import.meta.url))
     const files = await getAllFiles(normalizedDir)
 
-    for(const file of files) {
+    for (const file of files) {
       const blocks = await collectBlocks(file)
       const executedBlocks = await executeBlocks(blocks)
       logCheck(executedBlocks, file, values)
@@ -77,33 +75,33 @@ async function logCheck (executedBlocks, filename, values) {
 
   for (const result of executedBlocks) {
     if (result.exitCode === 0) {
-      results.pass.push({line: result.line, filename: filename})
+      results.pass.push({ line: result.line, filename })
     } else if (result.exitCode === 1) {
-      results.fail.push({line: result.line, filename: filename})
+      results.fail.push({ line: result.line, filename })
     }
   }
 
-  if(values.json && values.silent) {
+  if (values.json && values.silent) {
     throw new Error('You cannot use the --json and --silent flags together.')
   }
 
-  if(values.json === true) { // output JSON if 
+  if (values.json === true) { // output JSON if
     console.log(JSON.stringify(results, null, 2))
   }
 
   // TODO: rework styling to be nicer
-  if(values.silent !== true) {
+  if (values.silent !== true) {
     console.log(`\n${results.pass.length} passed, ${results.fail.length} failed\n`)
-    if(results.fail.length !== 0) {
+    if (results.fail.length !== 0) {
       console.log('Failed checks:')
-      for(const fail of results.fail) {
+      for (const fail of results.fail) {
         console.log(`- ${fail.filename}:${fail.line}`)
       }
     }
   }
 
-  if(results.fail.length !== 0) {
-    process.exitCode = 1 
+  if (results.fail.length !== 0) {
+    process.exitCode = 1
   } else {
     process.exitCode = 0
   }
