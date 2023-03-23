1b172ae9eae6fd0d030cf0db4aaf4c5167c30a50
Tierney Cyren <hello@bnb.im>
2023-03-23 22:58:25 +0900

    fix: run linter again
    Signed-off-by: Tierney Cyren <hello@bnb.im>

--- core/index.mjs
+++ core/index.mjs
@@ -14,37 +14,36 @@ export async function collectBlocks (file) {
 }
 
 export async function executeBlocks (tree) {
-    let placeholder = []
-    const result = new Promise((resolve, reject) => {
-      visit(tree, async function (node, index, parent) {
-        // handle the situation where there are no code blocks at all
-        if(node.type === 'root') {
-          if(node.children.findLastIndex(child => child.type === 'code') === -1) {
-            const mockParsed = await parser('', node)
-            placeholder.push(mockParsed)
-            resolve(placeholder)
-          }
-
-          // method we use to check if there are any code blocks that are useful.
-          const usefulLanguageBlocks = (node) => node.lang === 'js' || node.lang === 'javascript' || node.lang === 'cjs'
-          if(node.children.some(usefulLanguageBlocks) === false) {
-            const mockParsed = await parser('', node)
-            placeholder.push(mockParsed)
-            resolve(placeholder)
-          }
+  const placeholder = []
+  const result = new Promise((resolve, reject) => {
+    visit(tree, async function (node, index, parent) {
+      // handle the situation where there are no code blocks at all
+      if (node.type === 'root') {
+        if (node.children.findLastIndex(child => child.type === 'code') === -1) {
+          const mockParsed = await parser('', node)
+          placeholder.push(mockParsed)
+          resolve(placeholder)
         }
-        
-        if (node.type === 'code' && (node.lang === 'js' || node.lang === 'javascript' || node.lang === 'cjs')) {
 
-          const executed = spawn('node', ['--eval', `${node.value}`])
-          const parsed = await parser(executed, node)
-          placeholder.push(parsed)
-          if(index === parent.children.findLastIndex(finalCodeBlock => finalCodeBlock.type === 'code')) {
-            resolve(placeholder)
-          }
+        // method we use to check if there are any code blocks that are useful.
+        const usefulLanguageBlocks = (node) => node.lang === 'js' || node.lang === 'javascript' || node.lang === 'cjs'
+        if (node.children.some(usefulLanguageBlocks) === false) {
+          const mockParsed = await parser('', node)
+          placeholder.push(mockParsed)
+          resolve(placeholder)
         }
-      })
+      }
+
+      if (node.type === 'code' && (node.lang === 'js' || node.lang === 'javascript' || node.lang === 'cjs')) {
+        const executed = spawn('node', ['--eval', `${node.value}`])
+        const parsed = await parser(executed, node)
+        placeholder.push(parsed)
+        if (index === parent.children.findLastIndex(finalCodeBlock => finalCodeBlock.type === 'code')) {
+          resolve(placeholder)
+        }
+      }
     })
+  })
 
-    return result
+  return result
 }
